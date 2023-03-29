f8277636ae5b1572415dff234201ebb9771bc7bf
Adarsh Kumar <108090248+anonymousguy1507@users.noreply.github.com>
2023-03-29 22:14:50 +0530

    Update DP1_fibonacci_no.cpp

--- DP1_fibonacci_no.cpp
+++ DP1_fibonacci_no.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
+        //checking base case
         if(n<=1)
           return n;
+        // dp[0] = 0, dp[1] = 1 changes to prev2 and prev respectively.
         int prev2 = 0;
         int prev = 1;
+       // start from idx 2 to n 
         for(int i=2;i<=n;i++){
-            int cur_i = prev + prev2;
+            int cur_i = prev + prev2; // dp[i] = dp[i-1] + dp[i-2] -----> cur_i = prev + prev2;
             prev2 = prev;
             prev = cur_i;
         }
